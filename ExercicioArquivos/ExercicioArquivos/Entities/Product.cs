a6e4c75e799a7a1202a33a5ceb8cf8be8390d6d4
Kaua Menezes <kauamenezes6@gmail.com>
2026-01-03 23:19:45 -0300

    Adding the archives exercise

---
+++ ExercicioArquivos/ExercicioArquivos/Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using System.Globalization;
+
+namespace ExercicioArquivos.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;    
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string TotalValue()
+        {
+            return (Price * Quantity).ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {TotalValue()}";
+        }
+    }
+}
---
+++ ExercicioArquivos/ExercicioArquivos/Program.cs
@@ -0,0 +1,58 @@
+﻿using ExercicioArquivos.Entities;
+using System.Globalization;
+using ExercicioArquivos.Exceptions;
+
+namespace ExericioArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\PROJETOS\CSV archives\Exercise.csv";
+            string targetPath = @"D:\PROJETOS\CSV archives\Result.csv";
+
+
+            try
+            {
+                if (File.Exists(targetPath))
+                {
+                    throw new DomainException("The file already exists!");
+                }
+
+                List<Product> products = new List<Product>();
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] lineArg = lines[i].Split(',');
+                    products.Add(new Product(lineArg[0], double.Parse(lineArg[1], CultureInfo.InvariantCulture), int.Parse(lineArg[2])));
+                }
+
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (Product product in products)
+                    {
+                        sw.WriteLine(product);
+                    }
+                }
+
+
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("PROCCESS COMPLETED");
+                Console.WriteLine("----------------------------------------------");
+
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine($"An error ocurred: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unexpected error: {e.Message}");
+            }
+
+        }
+    }
+}
