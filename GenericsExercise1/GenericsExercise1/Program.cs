6a5be23c4982b6745d90ba0e5a1c6e156bdec745
Kaua Menezes <kauamenezes6@gmail.com>
2026-01-27 21:18:54 -0300

    first part of the exercise

---
+++ GenericsExercise1/GenericsExercise1/Program.cs
@@ -0,0 +1,39 @@
+﻿using GenericsExercise1.Services;
+using System;
+
+namespace GenericsExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PrintService service = new PrintService();
+
+                Console.Write("How many values? ");
+                int numberOfValues = int.Parse(Console.ReadLine());
+                if (numberOfValues < 1 || numberOfValues > 10)
+                {
+                    throw new InvalidOperationException("Number must be between 1 and 10");
+                }
+
+
+                for (int i = 1; i <= numberOfValues; i++)
+                {
+                    int value = int.Parse(Console.ReadLine());
+                    service.AddValue(value);
+                }
+
+                service.Print();
+
+                Console.WriteLine();
+
+                Console.WriteLine($"First: {service.First()}");
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ GenericsExercise1/GenericsExercise1/Services/PrintService.cs
@@ -0,0 +1,45 @@
+﻿
+namespace GenericsExercise1.Services
+{
+    internal class PrintService
+    {
+        public int[] Values = new int[10];
+        public int Count = 0;
+
+        public void AddValue(int value)
+        {
+            if (Count == 10)
+            {
+                throw new InvalidOperationException("Print service is full");
+            }
+            Values[Count] = value;
+            Count++;
+        }
+
+        public int First()
+        {
+            if (Values[0] == null)
+            {
+                throw new InvalidOperationException("Insert some values first");
+            }
+            return Values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write('[');
+
+            foreach (int value in Values)
+            {
+                if (value != 0)
+                {
+                    Console.Write($"{value}, ");
+                }
+                
+            }
+
+            Console.Write(']');
+
+        }
+    }
+}
