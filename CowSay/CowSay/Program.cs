e98467f78a8673f2485bb113e4fc971214716af4
Kaua Menezes <kauamenezes6@gmail.com>
2026-02-09 21:17:06 -0300

    Simple CowSay C# console application

---
+++ CowSay/CowSay/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace CowSay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What will the cow say? ");
+            string cowText = Console.ReadLine();
+
+            string barra = new string('-', cowText.Length + 2);
+            
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine(barra);
+            Console.WriteLine($"<{cowText}>");
+            Console.WriteLine(barra);
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("        \\   ^__^");
+            Console.WriteLine("         \\  (oo)\\_______");
+            Console.WriteLine("            (__)\\       )\\/\\");
+            Console.WriteLine("                ||----w |");
+            Console.WriteLine("                ||     ||");
+            Console.ResetColor();
+        }
+
+
+    }
+}
